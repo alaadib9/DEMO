b9af5e7620a56c090daae3efa984be73155012ba
‪alaadeeb deeb95‬‏ <alaadeebdeeb9955@gmail.com>
2020-12-28 15:29:11 +0200

    JS

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,53 @@
 "use strict";
+var myFirstQ = prompt ("Do you think I love animals? Yes or No")
+if (myFirstQ.toLowerCase === 'yes') {
+    alert('your guessing is false. I dont love animals :(')
+} else if (myFirstQ.toLowerCase === 'no') {
+    alert('You are rigth')
+} else {
+    alert('It is a yes no question make sure to choose one of them only')
+}
+
+alert('Time for the second question dear')
+
+var secQ = prompt("Have I ever lost someone close? Yes or No")
+if (secQ.toLowerCase === 'yes') {
+    alert('Yes, I lost :(')
+} else if (secQ.toLowerCase === 'no') {
+    alert('Your guessing is false ')
+} else {
+    alert('It is a yes no question make sure to choose one of them only')
+
+}
+
+var secQ = prompt("Do I like to exercise? Yes or No")
+if (secQ.toLowerCase === 'yes') {
+    alert('Yes I exercise every day ')
+} else if (secQ.toLowerCase === 'no') {
+    alert('You are incorrect :( I love sport :)')
+} else {
+    alert('It is a yes no question make sure to choose one of them only')
+
+}
+
+var secQ = prompt("Do I know how to play a musical instrument? Yes or No")
+if (secQ.toLowerCase === 'yes') {
+    alert('Unfortunately, I dont know how to play')
+} else if (secQ.toLowerCase === 'no') {
+    alert('You are correct ')
+} else {
+    alert('It is a yes no question make sure to choose one of them only')
+
+}
+
+var secQ = prompt("Have you cheated in a game? Yes or No")
+if (secQ.toLowerCase === 'yes') {
+    alert('For sure :)')
+} else if (secQ.toLowerCase === 'no') {
+    alert('are sure? of course i cheated :)')
+} else {
+    alert('It is a yes no question make sure to choose one of them only')
+
+}
+
+
